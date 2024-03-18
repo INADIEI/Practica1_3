442cc3173968396d660db7bf65a454b15dec7615
INADIEI <161649696+INADIEI@users.noreply.github.com>
2024-03-17 23:05:32 -0600

    Create ParteObligatoriaPractica.js
    Estos son los codigos que se encuentran adjuntos en la practica 1.3, se deben implementar en los puntos

---
+++ ParteObligatoriaPractica.js
@@ -0,0 +1,46 @@
+
+const size =new Size(1080,764);
+size.width;
+// => 1080
+size.height;
+// => 764
+
+size.resize(1920, 1080);
+siza.width;
+// => 1920
+size.height;
+// => 10180
+
+const point=new Possition();
+point.x;
+// => 0
+point.y;
+// => 0
+
+point.move(100,200);
+point.x;
+point.y;
+
+const programWindow = new ProgramWindow();
+programWindow.screenSize.width;
+// =>800
+
+const programWindow = new ProgramWindow();
+
+const newSize(600,400)
+programWindow.resize(newSize);
+programWindow.size.width;
+//=> 600
+programWindow.size.height;
+// =>400
+
+const programWindow = new ProgramWindow();
+
+const newPossition = new Possition(50,100);
+programWindow.move(newPossition);
+programWindow.position.x;
+programWindow.position.y;
+
+const programWindow = new ProgramWindow();
+changeWindow(programWindow);
+programWindow.size.width;
